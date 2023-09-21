c389092e5557543792ad0e04585cb1a5743ad1ba
enisbe <enisbe@gmail.com>
2023-09-20 22:42:34 -0600

    Update contains.py

--- contains.py
+++ contains.py
@@ -19,3 +19,49 @@ def group_contains_feature(df, group_col, feature_col, substring):
 # Call the function, searching for groups containing the substring "var"
 result = group_contains_feature(df, 'GROUP', 'FEATURE', 'var')
 print(result)
+
+# Create num of econ
+
+import pandas as pd
+
+# Sample dataframe
+df = pd.DataFrame({
+    'Model_ID': ['A', 'A', 'B', 'B', 'B', 'C', 'C'],
+    'FEATURE': ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7'],
+    'VAR_TYPE': ['ECON', 'ACCOUNT', 'ECON', 'ACCOUNT', 'OTHER', 'ACCOUNT', 'ECON']
+})
+
+def count_type(series, var_type):
+    """Custom function to count occurrences of a specific VAR_TYPE."""
+    return (series == var_type).sum()
+
+# Use groupby and aggregate to count occurrences of ECON and ACCOUNT types for each Model_ID
+result = df.groupby('Model_ID').agg(
+    ECON_COUNT=('VAR_TYPE', lambda x: count_type(x, 'ECON')),
+    ACCOUNT_COUNT=('VAR_TYPE', lambda x: count_type(x, 'ACCOUNT'))
+).reset_index()
+
+print(result)
+
+# Create HAS_RUN column 
+
+import pandas as pd
+
+# Given dataframes
+performance_df = pd.DataFrame({
+    'Model_ID': ['A', 'A', 'B', 'B', 'B', 'C', 'C'],
+    'FEATURE': ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7'],
+    'VAR_TYPE': ['ECON', 'ACCOUNT', 'ECON', 'ACCOUNT', 'OTHER', 'ACCOUNT', 'ECON']
+})
+
+data = {
+    'A': [1, 2, 3],
+    'B': [4, 5, 6],
+    'C': [7, 8, 9]
+}
+df = pd.DataFrame(data)
+
+# Create new column 'HAS_RUN' by checking if 'Model_ID' exists in df columns
+performance_df['HAS_RUN'] = performance_df['Model_ID'].isin(df.columns)
+
+print(performance_df)
