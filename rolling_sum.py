246a40ed2b05680dced9e49ae7f430039b60fe33
enisbe <enisbe@gmail.com>
2023-12-11 18:47:44 -0700

    Update rolling_sum.py

--- rolling_sum.py
+++ rolling_sum.py
@@ -1,15 +1,12 @@
-import numpy as np
 
-def rolling_sum(arr, min_periods=1, ascending=True, window_sizes=None):
+def rolling_sum2(arr, min_periods=1, ascending=True, window_size_arr = None):
     """
     Apply a rolling sum to each column of a 2D NumPy array, either ascending or descending.
-    If window_sizes array is provided, use the respective window size for each column. 
-    Otherwise, use the column index + 1 as the window size.
+    The window size for each column is determined by its index + 1.
 
     :param arr: A 2D NumPy array.
     :param min_periods: Minimum number of observations in window required to have a value.
     :param ascending: Boolean indicating the direction of rolling sum (True for ascending, False for descending).
-    :param window_sizes: Optional array of window sizes for each column.
     :return: A 2D NumPy array with rolling sums.
     """
     nrows, ncols = arr.shape
@@ -19,17 +16,21 @@ def rolling_sum(arr, min_periods=1, ascending=True, window_sizes=None):
     if not ascending:
         arr = np.flipud(arr)
 
+    if window_size_arr is None:
+        window_size_arr = list(range(ncols))
+    
     # Iterate over each column
     for col in range(ncols):
-        # Use provided window size for the column, or column index + 1 if not provided
-        current_window_size = window_sizes[col] if window_sizes is not None and col < len(window_sizes) else col + 1
+        
+        size = window_size_arr[col]
+        window_size = size + 1
         
         # Apply rolling sum
         for row in range(nrows):
-            if row < current_window_size - min_periods:
+            if row < window_size - min_periods:
                 result[row, col] = np.nan  # Not enough data to fill window
             else:
-                start = max(0, row - current_window_size + 1)
+                start = max(0, row - window_size + 1)
                 result[row, col] = arr[start:row + 1, col].sum()
 
     # Reverse result if descending
