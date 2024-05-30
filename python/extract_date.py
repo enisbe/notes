324368348b4458fd297f3f2bcc0dd0a451ecf57b
enisbe <enisbe@gmail.com>
2024-05-30 09:38:12 -0600

    Update extract_date.py

--- python/extract_date.py
+++ python/extract_date.py
@@ -1,9 +1,18 @@
 import re
+import os
 from datetime import datetime
 
-def extract_date_from_filename(filename):
+def extract_date_from_filename(filepath):
+    # Extract the filename from the full path
+    filename = os.path.basename(filepath)
+    
     # Define regex patterns for different date formats
     date_patterns = [
+        r'(\d{1,2})\.(\d{1,2})\.(\d{2})', # Matches '4.1.2023' or '04.01.2023'
+        r'(\d{4})-(\d{2})-(\d{2})',       # Matches '2023-04-01'
+        r'(\d{1,2})-(\d{1,2})-(\d{2})',   # Matches '4-1-2023' or '04-01-2023'
+        r'(\d{4})\.(\d{2})\.(\d{2})',     # Matches '2023.04.01'
+        r'(\d{1,2})/(\d{1,2})/(\d{2})',    # Matches '4/1/2023' or '04/01/2023'
         r'(\d{1,2})\.(\d{1,2})\.(\d{4})', # Matches '4.1.2023' or '04.01.2023'
         r'(\d{4})-(\d{2})-(\d{2})',       # Matches '2023-04-01'
         r'(\d{1,2})-(\d{1,2})-(\d{4})',   # Matches '4-1-2023' or '04-01-2023'
@@ -28,14 +37,15 @@ def extract_date_from_filename(filename):
     return None
 
 # Test cases
-filenames = [
-    'filename 4.1.2023.txt',
-    'filename_4.1.2023.txt',
-    'filename 2023-04-01.txt',
-    'someotherfilename 4.1.2023.txt',
-    'otherfilename 2023-04-01.txt'
+filepaths = [
+    f'c:\path\to\filename 12.7.2023.xlsx',
+    '/path/to/filename 4.1.2023.txt',
+    '/another/path/filename_4.1.2023.txt',
+    '/different/path/filename 2023-04-01.txt',
+    '/another/one/someotherfilename 4.1.2023.txt',
+    '/last/one/otherfilename 2023-04-01.txt'
 ]
 
-for fname in filenames:
-    extracted_date = extract_date_from_filename(fname)
-    print(f"Filename: {fname}, Extracted Date: {extracted_date}")
+for filepath in filepaths:
+    extracted_date = extract_date_from_filename(filepath)
+    print(f"Filepath: {filepath}, Extracted Date: {extracted_date}")
